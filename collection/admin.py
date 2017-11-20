856d7c12ec288c7f9313bab0431000aace714772
Kyle Chivers <chiverskyle@gmail.com>
2017-11-20 08:49:23 -0800

    added new model

--- collection/admin.py
+++ collection/admin.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from collection.models import Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+	model = Profile
+	list_display = ('name', 'description', 'slug',)
+	prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Profile, ProfileAdmin)
