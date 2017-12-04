bd4f05e4c6a309fc4939784f4217f0d917bca7f1
Kyle Chivers <chiverskyle@gmail.com>
2017-12-04 08:32:48 -0800

    view for model form added

--- collection/views.py
+++ collection/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from collection.forms import ProfileForm
 from collection.models import Profile
 
 # Create your views here.
@@ -13,3 +15,15 @@ def index(request):
 def profile_detail(request, slug):
 	profile = Profile.objects.get(slug=slug)
 	return render(request, 'collection/profile_detail.html', {'profile': profile,})
+
+def edit_profile(request, slug):
+	profile = Profile.objects.get(slug=slug)
+	form = ProfileForm(instance=profile)
+
+	if request.method == 'POST':
+		form = ProfileForm(data=request.POST, instance=profile)
+		if form.is_valid():
+			form.save()
+			return redirect('profile_detail', slug=profile.slug)
+
+	return render(request, 'collection/edit_profile.html', {'profile': profile, 'form': form,})
