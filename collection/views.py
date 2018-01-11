bf621f2d5827a3b6d354a59926b9d5d9ae0c4fb6
Kyle Chivers <chiverskyle@gmail.com>
2018-01-11 08:48:34 -0800

    added view for profile creation

--- collection/views.py
+++ collection/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.template.defaultfilters import slugify
 from django.shortcuts import render, redirect
 
 from collection.forms import ProfileForm
@@ -27,3 +28,28 @@ def edit_profile(request, slug):
 			return redirect('profile_detail', slug=profile.slug)
 
 	return render(request, 'collection/edit_profile.html', {'profile': profile, 'form': form,})
+
+def create_profile(request):
+	form_class = ProfileForm
+
+	if request.method == 'POST':
+		# get data from form
+		form = form_class(request.POST)
+		if form.is_valid():
+			# check if form is valid but don't save
+			# instance yet
+			profile = form.save(commit=False)
+
+			# set additional details
+			profile.user = request.user
+			profile.slug = slugify(profile.name)
+
+			# save the object
+			profile.save()
+
+			# redirect to newly created profile
+			return redirect('profile_detail', slug=profile.slug)
+	# otherwise create the form
+	else:
+		form = form_class()
+	return redirect(request, 'collection/create_profile.html', {'form': form,})
