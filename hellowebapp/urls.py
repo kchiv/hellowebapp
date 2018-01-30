0aaa85d014ebda80ff14535f901a30d739a78add
Kyle Chivers <chiverskyle@gmail.com>
2018-01-30 08:21:27 -0800

    url routing for password change

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -19,7 +19,9 @@ from django.contrib.auth.views import (
     password_reset,
     password_reset_done,
     password_reset_confirm,
-    password_reset_complete
+    password_reset_complete,
+    password_change,
+    password_change_done,
 )
 
 from django.conf.urls import url, include
@@ -35,6 +37,8 @@ urlpatterns = [
     url(r'^accounts/password/reset/$', password_reset, {'template_name': 'registration/password_reset_form.html'}, name='password_reset'),
     url(r'^accounts/password/reset/done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name='password_reset_done'),
     url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name='password_reset_confirm'),
+    url(r'^accounts/password/change/$', password_change, {'template_name': 'registration/password_change_form.html'}, name='password_change'),
+    url(r'^accounts/password/change/done/$', password_change_done, {'template_name': 'registration/password_change_done.html'}, name='password_change_done'),
     url(r'^accounts/password/done/$', password_reset_complete, {'template_name': 'registration/password_reset_complete.html'}, name='password_reset_complete'),
     url(r'^$', views.index, name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='collection/about.html'), name='about'),
